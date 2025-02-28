0e61d6ebcab686783e2a5a868a3063ddab318124
benSharon <omarmoukhtar@gmail.com>
2025-02-28 19:20:18 +0200

    updated methods.py

--- methods.py
+++ methods.py
@@ -3,6 +3,10 @@ import string
 def caesar_rotate(text, key):
     decrypted = []
     for char in text:
+        # Skipping non-alphabet chars
+        if char not in string.ascii_letters:
+            continue
+
         if char.islower():
             lower_char_index = string.ascii_lowercase.index(char)
             decrypted.append(string.ascii_lowercase[(lower_char_index + key) % 26])
@@ -11,6 +15,7 @@ def caesar_rotate(text, key):
             decrypted.append(string.ascii_uppercase[(upper_char_index + key) % 26])
         else:
             decrypted.append(char)
+
     print(''.join(decrypted))
 
 
@@ -19,11 +24,16 @@ def caesar_bruteforce(ciphertext):
     for shift in range(1, 26):
         decrypted = []
         for char in ciphertext:
+            # Skipping non-alphabet chars
+            if char not in string.ascii_letters:
+                continue
+
             if char.islower():
                 decrypted.append(string.ascii_lowercase[(string.ascii_lowercase.index(char) + shift) % 26])
             elif char.isupper():
                 decrypted.append(string.ascii_uppercase[(string.ascii_uppercase.index(char) + shift) % 26])
             else:
                 decrypted.append(char)
+
         print(f"Shift {shift:2d}: {''.join(decrypted)}")
         print("--------------------------------------------------")
